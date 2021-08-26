0226859f0d899c341a63f35fecad268f3d5e16d8
James Kwan <31811580+jamessykwan@users.noreply.github.com>
2021-08-25 23:54:46 -0700

    add form

--- todo/views.py
+++ todo/views.py
@@ -4,10 +4,31 @@ from .models import Task
 
 tasks = Task.objects.all()
 
+
 def home(request):
-    return render(request, "todo/home.html", {
-        "tasks": tasks
-    })
+     if request.method == 'POST':
+        if request.POST.get('title') and request.POST.get('content'):
+            post = Task()
+            post.name = request.POST.get('title')
+            post.content = request.POST.get('content')
+            post.save()
+            tasks = Task.objects.all()
+
+            return render(request, "todo/home.html", {
+                "tasks": tasks
+            })
+
+     else:
+        tasks = Task.objects.all()
+
+        return render(request, "todo/home.html", {
+            "tasks": tasks
+        })
+
 
 def add_task(request):
-    Task.objects.create(name='Task 2',content='hello')
+    Task.objects.create(name='Task 2', content='hello')
+
+
+
+   
