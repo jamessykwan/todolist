ebf00e02412eba93e92fd4d538265a574d963eb3
Trevor <trevt.sd@gmail.com>
2021-08-26 13:37:31 -0700

    clear kind of works still bugged

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,5 @@ from todo import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("clear/", views.clear_tasks, name="clear")
 ]
--- todo/views.py
+++ todo/views.py
@@ -4,30 +4,26 @@ from .models import Task
 
 tasks = Task.objects.all()
 
-
 def home(request):
-     if request.method == 'POST':
+    tasks = Task.objects.all()
+    if request.method == 'POST':
         if request.POST.get('title') and request.POST.get('content'):
-            post = Task()
-            post.name = request.POST.get('title')
-            post.content = request.POST.get('content')
-            post.save()
-            tasks = Task.objects.all()
+            name = request.POST.get('title')
+            content = request.POST.get('content')
+            add_task(name,content)
 
-            return render(request, "todo/home.html", {
-                "tasks": tasks
+    return render(request, "todo/home.html", {
+            "tasks": tasks 
             })
 
-     else:
-        tasks = Task.objects.all()
+def add_task(task_name, task_content):
+    Task.objects.create(name=task_name, content=task_content)
 
-        return render(request, "todo/home.html", {
+def clear_tasks(request):
+    Task.objects.all().delete()
+    return render(request, "todo/home.html", {
             "tasks": tasks
         })
-
-
-def add_task(request):
-    Task.objects.create(name='Task 2', content='hello')
 
 
 
