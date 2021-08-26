440e823dcf6eec30132020d0d82f5b9878dc228c
James Kwan <31811580+jamessykwan@users.noreply.github.com>
2021-08-26 15:34:49 -0700

    add task page

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,6 @@ from todo import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("clear/", views.clear_tasks, name="clear")
+    path("clear/", views.clear_tasks, name="clear"),
+    path("addTask/", views.addTask, name="addTask")
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django import forms
 from .models import Task
 
@@ -6,25 +6,29 @@ tasks = Task.objects.all()
 
 def home(request):
     tasks = Task.objects.all()
+    return render(request, "todo/home.html", {
+        "tasks": tasks
+    })
+
+
+def addTask(request):
     if request.method == 'POST':
         if request.POST.get('title') and request.POST.get('content'):
             name = request.POST.get('title')
             content = request.POST.get('content')
-            add_task(name,content)
+            add_task(name, content)
+            return redirect('home')
+    return render(request, "todo/addTask.html", {
+        "tasks": tasks
+    })
 
-    return render(request, "todo/home.html", {
-            "tasks": tasks 
-            })
 
 def add_task(task_name, task_content):
     Task.objects.create(name=task_name, content=task_content)
 
+
 def clear_tasks(request):
     Task.objects.all().delete()
     return render(request, "todo/home.html", {
-            "tasks": tasks
-        })
-
-
-
-   
+        "tasks": tasks
+    })
