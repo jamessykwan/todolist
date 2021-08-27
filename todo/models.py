2d8912a18720358b4f3677e4a02d8bc35d1a3695
Trevor <trevt.sd@gmail.com>
2021-08-27 15:04:50 -0700

    completed list

--- todo/models.py
+++ todo/models.py
@@ -6,6 +6,7 @@ class Task(models.Model):
     name = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
